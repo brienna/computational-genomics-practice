104cbc6d31788c9ea91aeca2ae229554017c502a
briennakh <briennakh@gmail.com>
2019-09-29 17:55:37 -0500

    Parse query and ref sequences

--- anchored_global_sequence_alignment/main.py
+++ anchored_global_sequence_alignment/main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from Bio import SeqIO
 
 def make_arg_parser():
 	'''Parses arguments.'''
@@ -8,7 +9,7 @@ def make_arg_parser():
 									 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('-q', '--query',
 						required=True,
-						default=argparse.SUPPRESS, # stop --help from displaying default: None
+						default=argparse.SUPPRESS, # no default value set, not even None
 						help='Path to query sequence fasta [required]')
 	parser.add_argument('-r', '--ref',
 						required=True,
@@ -16,13 +17,27 @@ def make_arg_parser():
 						help='Path to reference sequence fasta [required]')
 	parser.add_argument('-m', '--matches',
 						required=False,
-						default=argparse.SUPPRESS,
 						help='Path to matches text file')
 	return parser
 
+
+def run_nw(query, ref):
+	print(query.seq)
+	print(ref.seq)
+	
 
 if __name__ == '__main__':
 	# Parse command-line arguments
 	parser = make_arg_parser()
 	args = parser.parse_args()
+
+	# Parse query and ref data
+	query = list(SeqIO.parse(args.query, 'fasta'))[0]
+	ref = list(SeqIO.parse(args.ref, 'fasta'))[0]
+
+	# If matches file is given, 
+	if args.matches is None:
+		run_nw(query, ref)
+	else: 
+		print('Matches supplied')
 
